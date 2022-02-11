078fa6814f6b3b5e4f8b76dfba1f3454c0b63bf3
"Kareem Glover" <"iamkhova@gmail.com">
2022-02-10 19:36:32 -0500

    init

---
+++ contract-8accfa45c0.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts@4.4.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.4.2/security/Pausable.sol";
+import "@openzeppelin/contracts@4.4.2/access/Ownable.sol";
+
+contract MyToken is ERC20, Pausable, Ownable {
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 1000000000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
